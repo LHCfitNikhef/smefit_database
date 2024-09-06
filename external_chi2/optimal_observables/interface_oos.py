638bc7acd732b2a343d1e43677248e299e0cc0af
Luca Mantani <luca.mantani@gmail.com>
2024-09-06 16:43:36 +0100

    Added RGE mat

--- external_chi2/optimal_observables/interface_oos.py
+++ external_chi2/optimal_observables/interface_oos.py
@@ -5,14 +5,14 @@ import jax.numpy as jnp
 current_file_path = pathlib.Path(__file__).resolve().parent
 
 # future colliders to include
-collider = 'FCCee'
+collider = "FCCee"
 
 
 class OptimalWW:
 
     def __init__(self, coefficients, rgemat=None):
 
-        oo_wc_basis = ['OpD', 'OpWB', 'OWWW', 'Opl1', 'Ope', 'O3pl1']
+        oo_wc_basis = ["OpD", "OpWB", "OWWW", "Opl1", "Ope", "O3pl1"]
 
         self.project = np.zeros((len(oo_wc_basis), coefficients.size))
         for i, op in enumerate(oo_wc_basis):
@@ -20,12 +20,12 @@ class OptimalWW:
                 self.project[i, np.argwhere(coefficients.name == op)[0, 0]] = 1
 
         self.datasets = {
-            '{collider}_ww_lepto_161': 'invcov_{collider}_ww_leptonic_161.dat',
-            '{collider}_ww_lepto_240': 'invcov_{collider}_ww_leptonic_240.dat',
-            '{collider}_ww_lepto_365': 'invcov_{collider}_ww_leptonic_365.dat',
-            '{collider}_ww_semilep_161': 'invcov_{collider}_ww_semilep_161.dat',
-            '{collider}_ww_semilep_240': 'invcov_{collider}_ww_semilep_240.dat',
-            '{collider}_ww_semilep_365': 'invcov_{collider}_ww_semilep_365.dat'
+            "{collider}_ww_lepto_161": "invcov_{collider}_ww_leptonic_161.dat",
+            "{collider}_ww_lepto_240": "invcov_{collider}_ww_leptonic_240.dat",
+            "{collider}_ww_lepto_365": "invcov_{collider}_ww_leptonic_365.dat",
+            "{collider}_ww_semilep_161": "invcov_{collider}_ww_semilep_161.dat",
+            "{collider}_ww_semilep_240": "invcov_{collider}_ww_semilep_240.dat",
+            "{collider}_ww_semilep_365": "invcov_{collider}_ww_semilep_365.dat",
         }
 
         incovs_reordered = []
@@ -37,37 +37,19 @@ class OptimalWW:
 
         self.rgemat = rgemat
 
+        if self.rgemat is not None:
+            # multiply the RGE matrix as well
+            self.incov_tot = jnp.einsum(
+                "ij, jk, kl", self.rgemat.T, self.incov_tot, self.rgemat
+            )
+
         self.n_dat = len(oo_wc_basis)
 
     def compute_chi2(self, coefficient_values):
 
-        chi2_value = 0
-        for invcov_path in self.datasets.values():
-            invcov = np.loadtxt(
-                current_file_path / invcov_path.format(collider=collider)
-            )
-            if self.rgemat is not None:
-                chi2_value += jnp.linalg.multi_dot(
-                    [
-                        coefficient_values,
-                        self.rgemat.T,
-                        self.project.T,
-                        invcov,
-                        self.project,
-                        self.rgemat,
-                        coefficient_values,
-                    ]
-                )
-            else:
-                chi2_value += jnp.linalg.multi_dot(
-                    [
-                        coefficient_values,
-                        self.project.T,
-                        invcov,
-                        self.project,
-                        coefficient_values,
-                    ]
-                )
+        chi2_value = jnp.einsum(
+            "i, ij, j", coefficient_values, self.incov_tot, coefficient_values
+        )
 
         return chi2_value
 
@@ -76,16 +58,14 @@ class Optimaltt:
 
     def __init__(self, coefficients, rgemat=None):
 
-        oo_tt_wc_basis = ['OpQM', 'Opt', 'OtW', 'OtZ']
+        oo_tt_wc_basis = ["OpQM", "Opt", "OtW", "OtZ"]
 
         self.project = np.zeros((len(oo_tt_wc_basis), coefficients.size))
         for i, op in enumerate(oo_tt_wc_basis):
             if op in coefficients.name:
                 self.project[i, np.argwhere(coefficients.name == op)[0, 0]] = 1
 
-        self.datasets = {
-            '{collider}_tt_365': 'invcov_{collider}_tt_365GeV.dat'
-        }
+        self.datasets = {"{collider}_tt_365": "invcov_{collider}_tt_365GeV.dat"}
 
         incovs_reordered = []
         for path in self.datasets.values():
@@ -96,42 +76,18 @@ class Optimaltt:
 
         self.rgemat = rgemat
 
+        if self.rgemat is not None:
+            # multiply the RGE matrix as well
+            self.incov_tot = jnp.einsum(
+                "ij, jk, kl", self.rgemat.T, self.incov_tot, self.rgemat
+            )
+
         self.n_dat = len(oo_tt_wc_basis)
 
     def compute_chi2(self, coefficient_values):
 
-        chi2_value = 0
-        for invcov_path in self.datasets.values():
-            invcov = np.loadtxt(
-                current_file_path / invcov_path.format(collider=collider)
-            )
-            if self.rgemat is not None:
-                chi2_value += jnp.linalg.multi_dot(
-                    [
-                        coefficient_values,
-                        self.rgemat.T,
-                        self.project.T,
-                        invcov,
-                        self.project,
-                        self.rgemat,
-                        coefficient_values,
-                    ]
-                )
-            else:
-                chi2_value += jnp.linalg.multi_dot(
-                    [
-                        coefficient_values,
-                        self.project.T,
-                        invcov,
-                        self.project,
-                        coefficient_values,
-                    ]
-                )
+        chi2_value = jnp.einsum(
+            "i, ij, j", coefficient_values, self.incov_tot, coefficient_values
+        )
 
         return chi2_value
-
-
-
-
-
-
