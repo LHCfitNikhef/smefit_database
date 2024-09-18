27ceb873c3ba0b320e2f80946bf33b5a45e9c179
Alejo Rossia <alejo.r94@gmail.com>
2024-09-18 11:00:23 +0100

    Scales checked for inclusive datasets
    Finished the scale_checker.py code. No inclusive data file needs to be corrected.

--- theory/scale_checker.py
+++ theory/scale_checker.py
@@ -11,7 +11,7 @@ mz=91.1876
 mw=80.387
 
 ## patterns to match file names of inclusive datasets
-atlasInc="^ATLAS\_(\w+)[\S+](inc)[\S+]?"
+atlasInc="^ATLAS\_([A-Za-z0-9]+)\w+[\s\S+](inc)[\S+]?"
 cepcInc="^CEPC\_[\w+]?[\S+]?([0-9]{3})(GeV)?[\w+]?[\S+]?"
 cepcW="^CEPC\_[WB]"
 cepcZ="^CEPC\_[Z]"
@@ -20,7 +20,7 @@ fcceeInc="^FCCee\_[\w+]?[\S+]?([0-9]{3})(GeV)?[\w+]?[\S+]?"
 fcceeW="^FCCee\_[WB]"
 fcceeZ="^FCCee\_[Z]"
 fcceeA="^FCCee\_a"
-cmsInc="^CMS\_(\w+)[\S+](inc)[\S+]?"
+cmsInc="^CMS\_([A-Za-z0-9]+)\w+[\s\S+](inc)[\S+]?"
 ## Dataset groups
 allData=[atlasInc,cepcInc,cepcW,cepcZ,cepcA,fcceeInc,fcceeW,fcceeZ,fcceeA,cmsInc]
 leptoInclusive=[cepcInc,fcceeInc]
@@ -65,7 +65,7 @@ for file in filesToCheck:
         data=json.load(fi)
         ## check that there are as many scales as datapoints
         lenscales=len(data['scales'])
-        lensm=len(data['bestsm'])
+        lensm=len(data['best_sm'])
         if(lenscales!=lensm):
             with open(outputfile,'a') as out:
                 out.write(file + ' #Corrupt file, wrong quantity of scales')
@@ -81,7 +81,8 @@ for file in filesToCheck:
                     out.write(file + ' #Corrupt file, wrong scale')
                     continue
             print("File: {} OK".format(file))
-        ## start with lepton colliders Z data
+            continue
+        ## lepton colliders Z data
         com_patterns = [re.compile(pattern) for pattern in leptoZdata]
         listmatch = [pattern.search(file) for pattern in com_patterns]
         if any(listmatch):
@@ -91,7 +92,8 @@ for file in filesToCheck:
                     out.write(file + ' #Corrupt file, wrong scale')
                     continue
             print("File: {} OK".format(file))
-        ## start with lepton colliders pole data
+            continue
+        ## lepton colliders pole data
         com_patterns = [re.compile(pattern) for pattern in leptoAlpha]
         listmatch = [pattern.search(file) for pattern in com_patterns]
         if any(listmatch):
@@ -101,14 +103,32 @@ for file in filesToCheck:
                     out.write(file + ' #Corrupt file, wrong scale')
                     continue
             print("File: {} OK".format(file))
-        ## continue with inclusive lepton colliders
+            continue
+        ## inclusive lepton colliders
         com_patterns = [re.compile(pattern) for pattern in leptoInclusive]
         listmatch = [pattern.search(file) for pattern in com_patterns]
         if any(listmatch):
-             scale=float(listmatch[listmatch.index(True)].group(-1))
-             if any(scale!=scaFile for scaFile in data['scales']):
+            matchedObject=next((item for item in listmatch if isinstance(item,re.Match)),None)
+            scale=float(matchedObject.group(1))
+            if any(scale!=scaFile for scaFile in data['scales']):
                 with open(outputfile,'a') as out:
                     out.write(file + ' #Corrupt file, wrong scale')
                     continue
-             print("File: {} OK".format(file))
-                                    
+            print("File: {} OK".format(file))
+            continue
+        ## ATLAS/CMS inclusive
+        com_patterns = [re.compile(pattern) for pattern in lhcInclusive]
+        listmatch = [pattern.search(file) for pattern in com_patterns]
+        if any(listmatch):
+            matchedObject=next((item for item in listmatch if isinstance(item,re.Match)),None)
+            process=matchedObject.group(1)
+#           print(process) ##debugging
+            if process not in list(scaleset.keys()): continue
+#           if not(process in scaleset.keys): continue
+            print("ATLAS/CMS inclusive file = {} , process id = {}".format(file,process)) ##not necessary but informative
+            scale=scaleset[process]
+            if any(scale!=scaFile for scaFile in data['scales']):
+                with open(outputfile,'a') as out:
+                    out.write(file + ' #Corrupt file, wrong scale')
+                    continue
+            print("File: {} OK".format(file))
