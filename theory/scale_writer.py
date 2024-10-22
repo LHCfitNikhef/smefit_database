6711bedb7fb377b6cba586f1a9cce9629afaf417
jacoterh <54140851+jacoterh@users.noreply.github.com>
2024-10-22 16:15:19 +0100

    automatised scales for CMS

--- theory/scale_writer.py
+++ theory/scale_writer.py
@@ -17,8 +17,29 @@ def scale_ttZ(bin):
 def scale_ggF(bin):
     return np.sqrt(mh ** 2 + bin ** 2) # bin = pT_H
 
+def scale_H_VBF(bin):
+    return scale_ggF(bin)
+
+def scale_tt(bin):
+    return bin
+
+def scale_wz(bin):
+    return np.sqrt((mz + mw) ** 2 + bin ** 2)
+
+def scale_tz(bin):
+    return mt + mz
+
+def scale_tw(bin):
+    return mt + mw
+
 # collect all scale choices in dictionary with process name as key
-scale_funct_dict = {'ttZ': scale_ttZ, 'ggF': scale_ggF}
+scale_funct_dict = {'ttZ': scale_ttZ,
+                    'ggF': scale_ggF,
+                    'H': scale_ggF,
+                    'tt2D': scale_tt,
+                    'tt': scale_tt,
+                    'WZ': scale_wz,
+                    'tZ': scale_tz}
 
 def compute_scale(commondata):
     """
@@ -51,12 +72,27 @@ def compute_scale(commondata):
 
 scales_dict = {}
 path_to_commondata = pathlib.Path('../commondata')
+theory_base_path = pathlib.Path('../theory')
 for commondata_file in path_to_commondata.iterdir():
     with open(commondata_file, encoding="utf-8") as f:
         commondata = yaml.safe_load(f)
 
-        scales = compute_scale(commondata)
-        scales_dict[commondata['dataset_name']] = scales
+        if "bins" in commondata:
+            scales = compute_scale(commondata)
+
+            path_to_theory = theory_base_path / f"{commondata_file.stem}.json"
+
+            # add scales to theory file
+            with open(path_to_theory) as f_theory:
+                theory_config = json.load(f_theory)
+                theory_config['scales'] = scales
+
+            # dump theory file
+            with open(path_to_theory, "w") as f:
+                json.dump(theory_config, f, indent=1)
+
+            scales_dict[commondata['dataset_name']] = scales
+
 
 
 
