52e78f4944e29852d1097ce580ace9fc25bb5528
Tommaso Armadillo <tommaso.armadillo@gmail.com>
2025-06-06 10:48:23 +0200

    Explicited parameters in DY ext likelihood constructor, moved NLO to NLO_QCD

--- external_chi2/drell_yan/CMS_DYMee_13TeV.py
+++ external_chi2/drell_yan/CMS_DYMee_13TeV.py
@@ -4,7 +4,7 @@ from smefit import compute_theory as pr
 from smefit.rge.rge import load_rge_matrix
 
 class CMS_DYMee_13TeV:
-    def __init__(self, coefficients, rge_dict=None, **kwargs):
+    def __init__(self, coefficients, rge_dict=None, use_quad=True, order='LO'):
 
         operators = {
             k: {"max": 0.0, "min": 0.0} for k in coefficients.name
@@ -14,7 +14,7 @@ class CMS_DYMee_13TeV:
             rgematrix, operators_to_keep = load_rge_matrix(
                 rge_dict=rge_dict,
                 coeff_list=list(operators.keys()),
-                datasets=[{'name': 'CMS_DYMee_13TeV', 'order': kwargs['order']}],
+                datasets=[{'name': 'CMS_DYMee_13TeV', 'order': order}],
                 theory_path='./theory',
                 cutoff_scale=None,
                 result_path='./results',
@@ -26,13 +26,13 @@ class CMS_DYMee_13TeV:
 
         self.drell_yan_dataset = loader.load_datasets(
             commondata_path='./commondata',
-            datasets=[{'name': 'CMS_DYMee_13TeV', 'order': kwargs['order']}],
+            datasets=[{'name': 'CMS_DYMee_13TeV', 'order': order}],
             operators_to_keep=operators_to_keep,
-            use_quad=kwargs['use_quad'],
+            use_quad=use_quad,
             use_theory_covmat=True,
             use_t0=False,
             use_multiplicative_prescription=False,
-            default_order=kwargs['order'],
+            default_order=order,
             theory_path='./theory',
             rot_to_fit_basis=None,
             has_uv_couplings=False,
@@ -40,7 +40,7 @@ class CMS_DYMee_13TeV:
             rgemat=rgematrix,
             cutoff_scale=None,
         )
-        self.use_quad = kwargs['use_quad']
+        self.use_quad = use_quad
 
     def compute_chi2(self, coefficient_values):
         # Compute theory predictions
