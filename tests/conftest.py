1483481fb2bdbd883bc11cbeaa02cc38a8e242ff
Luca Mantani <luca.mantani@gmail.com>
2025-08-22 13:03:03 +0100

    Added check FCC optimal obs

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+# If src layout:
+from pathlib import Path
+import sys
+
+# add the path of the root, so that we can import python modules in the tests
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
